c94499e024f5840a365d8c2f32063e9b55b37b38
bhmberkan <berkanburakturgut@gmail.com>
2025-01-08 21:33:42 +0300

    Temel Entityler oluşturuldu

---
+++ Core/Hepsiapi.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using Hepsiapi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hepsiapi.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int BrandId{ get; set; }
+        public required decimal Price { get; set; }
+        public required decimal Discount { get; set; }
+        
+        public Brand Brand { get; set; } // tekil olacağını belirttik
+
+        public ICollection<Category> Categories { get; set; }
+        
+        //   public required string ImagePath{ get; set; }
+    }
+}

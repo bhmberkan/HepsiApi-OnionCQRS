6fa04b38f73e9d8b74becd0b6f0cebe89bda3f6d
bhmberkan <berkanburakturgut@gmail.com>
2025-02-05 13:34:02 +0300

    RefreshToken

---
+++ Core/Hepsiapi.Application/Features/Auth/Command/RefreshToken/RefreshTokenCommadValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hepsiapi.Application.Features.Auth.Command.RefreshToken
+{
+    public class RefreshTokenCommadValidator : AbstractValidator<RefreshTokenCommadRequest>
+    {
+        public RefreshTokenCommadValidator()
+        {
+            RuleFor(x => x.AcessToken).NotEmpty();
+            RuleFor(x => x.RefreshToken).NotEmpty();
+        }
+    }
+}
--- Core/Hepsiapi.Application/Features/Auth/Exceptions/EmailOrPasswordShouldNotBeInvalid.cs
+++ Core/Hepsiapi.Application/Features/Auth/Exceptions/EmailOrPasswordShouldNotBeInvalid.cs
@@ -5,7 +5,8 @@ namespace Hepsiapi.Application.Features.Auth.Exceptions
     public class EmailOrPasswordShouldNotBeInvalid : BaseException
     {
         public EmailOrPasswordShouldNotBeInvalid() : base("Kullanıcı adı veya şifre yamlıştır") { }
+       
     }
 
-
-}
+     
+    }

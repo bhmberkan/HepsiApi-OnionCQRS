1fec9cc2ad6472a5c5028c5edf80babd7a60f387
bhmberkan <berkanburakturgut@gmail.com>
2025-02-06 22:38:44 +0300

    Redis Yapısı

--- Core/Hepsiapi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryRequest.cs
+++ Core/Hepsiapi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryRequest.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Hepsiapi.Application.Interfaces.RedisCache;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,10 @@ using System.Threading.Tasks;
 
 namespace Hepsiapi.Application.Features.Products.Queries.GetAllProducts
 {
-    public class GetAllProductsQueryRequest : IRequest<IList<GetAllProductsQueryRepsonse>>
+    public class GetAllProductsQueryRequest : IRequest<IList<GetAllProductsQueryRepsonse>>, ICacheableQuery
     {
+        public string CacheKey => "GetAllProducts";
 
+        public double CacheTime => 60;
     }
 }
--- Core/Hepsiapi.Application/Registration.cs
+++ Core/Hepsiapi.Application/Registration.cs
@@ -34,6 +34,7 @@ namespace Hepsiapi.Application
             ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr");
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehevior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RedisCacheBehevior<,>));
         }
 
         private static IServiceCollection AddRulesFromAssemblyContaining(
